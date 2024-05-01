73b8a418831b35af3550fff7987e4e2cfd71c7be
Guilherme Lima <guilherme.lima@flexconsulta.com.br>
2024-05-01 12:30:49 -0300

    chore: configurando git hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-red-500 p-24">
       <h1>Food FullStack Week</h1>
     </main>
   );
